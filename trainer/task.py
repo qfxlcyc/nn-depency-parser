068d7e028b483536a9b338f9058e82dfd8ff697b
Quan Yuan <yq911122@gmail.com>
2018-10-08 14:14:05 -0700

    Clean task.py

--- trainer/task.py
+++ trainer/task.py
@@ -6,14 +6,15 @@ from main import train
 flags = tf.flags
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-project_dir = "./"
-data_dir = os.path.join(project_dir, "data")
+project_dir = "gs://nn-parser"
+# project_dir = "./"
+data_dir = os.path.join(project_dir, "data_all")
 log_dir = os.path.join(project_dir, "log/event")
 save_dir = os.path.join(project_dir, "log/model")
 
-train_file = os.path.join(data_dir, "train.csv")
-dev_file = os.path.join(data_dir, "dev.csv")
-test_file = os.path.join(data_dir, "test.csv")
+train_file = os.path.join(data_dir, "train")
+dev_file = os.path.join(data_dir, "dev")
+test_file = os.path.join(data_dir, "test")
 word_emb_file = os.path.join(data_dir, "en-cw.txt")
 meta_file = os.path.join(data_dir, "meta.json")
 
@@ -35,26 +36,25 @@ flags.DEFINE_integer("capacity", 15000, "Batch size of dataset shuffle")
 flags.DEFINE_integer("num_threads", 4, "Number of threads in input pipeline")
 
 flags.DEFINE_integer("hidden", 200, "Hidden layer dimension")
-flags.DEFINE_integer("out_dim", 3, "Parser output dimension")
 
 flags.DEFINE_integer("batch_size", 64, "Batch size")
 flags.DEFINE_integer("num_steps", 10000, "Number of steps")
 flags.DEFINE_integer("checkpoint", 1000, "checkpoint for evaluation")
 flags.DEFINE_integer("period", 50, "period to save batch loss")
-flags.DEFINE_float("init_lr", 0.01, "Initial lr for Adadelta")
+flags.DEFINE_float("init_lr", 1e-2, "Initial lr for Adadelta")
+flags.DEFINE_integer("val_num_batches", 30, "Num of batches for evaluation")
 flags.DEFINE_float("req_coeff", 10e-8, "coefficient of l2 loss")
 flags.DEFINE_float("keep_prob", 0.5, "Keep prob in model")
-flags.DEFINE_float("grad_clip", 5.0, "Global Norm gradient clipping rate")
-flags.DEFINE_integer("patience", 3, "Patience for lr decay")
-
+flags.DEFINE_float("grad_clip", 1e-1, "Global Norm gradient clipping rate")
 
 def main(_):
     config = flags.FLAGS
     if config.mode == "train":
         train(config)
     elif config.mode == "debug":
-        config.train_file = os.path.join(data_dir, "debug.csv")
-        config.dev_file = os.path.join(data_dir, "debug.csv")
+        config.train_file = os.path.join(data_dir, "debug")
+        config.dev_file = os.path.join(data_dir, "debug")
+        config.batch_size = 2
         config.num_steps = 2
         config.checkpoint = 2
         config.period = 1
