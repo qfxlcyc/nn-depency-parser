93bebcc6f54f45285707d5f120eb1c698c7c1032
Quan Yuan <yq911122@gmail.com>
2018-10-08 14:14:30 -0700

    Fix model hidden layer

--- trainer/model.py
+++ trainer/model.py
@@ -3,70 +3,96 @@ import json
 import data_helper
 
 def dropout(inputs, keep_prob, is_train=True):
-    return tf.cond(tf.cast(is_train, tf.bool), lambda: tf.nn.dropout(inputs, keep_prob),
-        lambda: inputs)
-
+    if is_train: return tf.nn.dropout(inputs, keep_prob)
+    else: return inputs
 
 class ParsingModel(object):
     """nn dependency parser"""
 
     def __init__(self, config, batch, word_mat=None, is_train=True):
-        self.meta = data_helper.get_data_meta(config)
-        self.is_train = tf.get_variable(
-            "is_train", shape=[], dtype=tf.bool, trainable=False)
+        self.meta = data_helper.load_json_from_gs(config.meta_file)
+        self.is_train = is_train
         self.word_mat = tf.get_variable("word_mat", initializer=tf.constant(
             word_mat, dtype=tf.float32), trainable=True)
-        self.pos_mat = tf.get_variable("pos_mat", shape=[self.meta["pos_class"]+1, config.embed_dim], initializer=tf.truncated_normal_initializer(stddev=0.01), 
-            trainable=True)
+        self.pos_mat = tf.get_variable("pos_mat", shape=[self.meta["num_pos_class"], config.embed_dim], initializer=tf.random_uniform_initializer(-0.01, 0.01), trainable=True)
+        self.arc_mat = tf.get_variable("arc_mat", shape=[self.meta["num_arc_class"], config.embed_dim], initializer=tf.random_uniform_initializer(-0.01, 0.01), trainable=True)
         self.global_step = tf.get_variable('global_step', shape=[], dtype=tf.int32,
                                            initializer=tf.constant_initializer(0), trainable=False)
-        self.inference(config, batch)
+        self.build(config, batch)
 
         if is_train:
             self.lr = tf.get_variable(
                 "lr", shape=[], dtype=tf.float32, trainable=False)
-            self.opt = tf.train.AdadeltaOptimizer(
-                learning_rate=self.lr, epsilon=1e-6)
+            # self.opt = tf.train.AdamOptimizer(learning_rate=self.lr, epsilon=1e-6)
+            self.opt = tf.train.AdagradOptimizer(learning_rate=self.lr)
             grads = self.opt.compute_gradients(self.loss)
             gradients, variables = zip(*grads)
             capped_grads, _ = tf.clip_by_global_norm(
                 gradients, config.grad_clip)
             self.train_op = self.opt.apply_gradients(
-                zip(capped_grads, variables), global_step=self.global_step)     
+                zip(capped_grads, variables), global_step=self.global_step)
+            # beta1_power, beta2_power = self.opt._get_beta_accumulators()
+            # self._lr = (self.opt._lr_t * tf.sqrt(1 - beta1_power) / (1 - beta2_power))
 
-    def inference(self, config, batch):
+            summ = []
+            for g, v in grads:
+                if g is not None:
+                    #print(format(v.name))
+                    grad_hist_summary = tf.summary.histogram("{}/grad_histogram".format(v.name), g)
+                    # sparsity_summary = tf.summary.scalar("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                    mean_summary = tf.summary.scalar("{}/grad_mean".format(v.name), tf.reduce_mean(g))
+                    summ.append(grad_hist_summary)
+                    # summ.append(sparsity_summary)
+                    # summ.append(mean_summary)
+                    summ.append(mean_summary)
+            self.grad_summ = tf.summary.merge(summ)
 
-        w, p, y = batch.get_next()
-        l2_loss = 0
+        self.saver = tf.train.Saver()  
+
+    def build(self, config, batch):
+
+        w, p, a, y = batch.get_next()
+        # w, y = batch.get_next()
+        l2_loss = 0.
 
         with tf.variable_scope("embedding"):
             with tf.variable_scope("word"):
                 w_emb = tf.nn.embedding_lookup(self.word_mat, w)
-                w_emb = tf.reshape(w_emb, shape=[config.batch_size, self.meta["word_feature_dim"] * config.embed_dim])
+                w_emb_size = self.meta["word_feature_dim"] * config.embed_dim
+                w_emb = tf.reshape(w_emb, shape=[config.batch_size, w_emb_size])
+                l2_loss += tf.nn.l2_loss(w_emb)
 
             with tf.variable_scope("pos"):
                 p_emb = tf.nn.embedding_lookup(self.pos_mat, p)
-                p_emb = tf.reshape(p_emb, shape=[config.batch_size, self.meta["pos_feature_dim"] * config.embed_dim])
-            emb = tf.concat([w_emb, p_emb], axis=1)
-            emb_size = emb.get_shape().as_list()[1]
-            l2_loss += tf.nn.l2_loss(emb)
+                p_emb_size = self.meta["pos_feature_dim"] * config.embed_dim
+                p_emb = tf.reshape(p_emb, shape=[config.batch_size, p_emb_size])
+                l2_loss += tf.nn.l2_loss(p_emb)
+
+            with tf.variable_scope("arc"):
+                a_emb = tf.nn.embedding_lookup(self.arc_mat, a)
+                a_emb_size = self.meta["arc_feature_dim"] * config.embed_dim
+                a_emb = tf.reshape(a_emb, shape=[config.batch_size, a_emb_size])
+                l2_loss += tf.nn.l2_loss(a_emb)
+            embeddings = [w_emb, p_emb, a_emb]
 
         with tf.variable_scope("hidden"):
-            W = tf.get_variable("W", shape=[emb_size, config.hidden], initializer=tf.truncated_normal_initializer(stddev=0.01))
-            b = tf.get_variable("b", shape=[config.hidden], initializer=tf.constant_initializer(0.))
-            h = tf.nn.xw_plus_b(emb, W, b) ** 3
+            W_w = tf.get_variable("W_word", shape=[w_emb_size, config.hidden], initializer=tf.truncated_normal_initializer(stddev=0.01))
+            W_p = tf.get_variable("W_pos", shape=[p_emb_size, config.hidden], initializer=tf.truncated_normal_initializer(stddev=0.01))
+            W_a = tf.get_variable("W_arc", shape=[a_emb_size, config.hidden], initializer=tf.truncated_normal_initializer(stddev=0.01))
+            W = [W_w, W_p, W_a]
+            b = tf.get_variable("b1", shape=[config.hidden], initializer=tf.constant_initializer(0.))
+            h = tf.pow(tf.add_n([tf.matmul(emb, w) for emb, w in zip(embeddings, W)]) + b, 3)
             h = dropout(h, config.keep_prob, self.is_train)
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
+            l2_loss += tf.nn.l2_loss(W_w)
 
         with tf.variable_scope("output"):
-            W = tf.get_variable("W", shape=[config.hidden, config.out_dim], initializer=tf.truncated_normal_initializer(stddev=0.01))
-            self.prob = tf.nn.softmax(tf.matmul(h, W))
-            l2_loss += tf.nn.l2_loss(W)
+            W2 = tf.get_variable("W2", shape=[config.hidden, self.meta["label_dim"]], initializer=tf.truncated_normal_initializer(stddev=0.01))
+            b2 = tf.get_variable("b2", shape=[self.meta["label_dim"]], initializer=tf.constant_initializer(0.))
+            self.prob = tf.nn.softmax(tf.nn.xw_plus_b(h, W2, b2))
+            l2_loss += tf.nn.l2_loss(W2)
 
             loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=self.prob)
-            losses = loss + config.req_coeff / 2.0 * l2_loss
-            self.loss = tf.reduce_mean(losses, name="loss")
+            self.loss = tf.reduce_mean(loss) + config.req_coeff / 2.0 * tf.reduce_sum(l2_loss)
 
             self.y = tf.argmax(self.prob, 1)
             correct_prediction = tf.equal(self.y, tf.argmax(y, 1))
